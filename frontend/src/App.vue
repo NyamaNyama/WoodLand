4cfa1508c2dc017709a435ffc5b906ed4f462184
Nyama N <>
2023-11-28 20:02:17 +1000

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>|
+    <router-link to="/sign-up">Sign up</router-link> |
+    <router-link to="/log-in">Log in</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<script>
+import axios from 'axios';
+ export default{
+  name:'App',
+  beforeCreate(){
+    this.$store.commit('initializeStore')
+
+    const access = this.$store.state.access
+
+    if(access){
+      axios.defaults.header.common['Authorization'] = "JWT "+ access
+    }else{
+      axios.defaults.headers.common['Authorization'] = ''
+    }
+  }
+ }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ frontend/src/views/SignUp.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="sign-up-page">
+            <h1>Sign up</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import axios from 'axios';
+
+export default{
+    name:'SignUp'
+}
+</script>
