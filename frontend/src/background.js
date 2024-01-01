0d2589072431c694d596b9f2cdc8600a29a181af
Anyona228 <Anyona>
2024-01-01 22:46:39 +1000

    electron stuff

---
+++ frontend/src/background.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var electron_1 = require("electron");
+function createWindow() {
+    var win = new electron_1.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+    win.loadURL('https://tsc');
+    win.maximize();
+    win.removeMenu();
+}
+electron_1.app.whenReady().then(function () {
+    createWindow();
+    electron_1.app.on('activate', function () {
+        if (electron_1.BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+electron_1.app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        electron_1.app.quit();
+    }
+});
